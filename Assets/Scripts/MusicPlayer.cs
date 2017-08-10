67c1d4d77627a30004c469327375593bfe566a0e
Scott Young <young.scottw@gmail.com>
2017-08-10 17:44:39 -0500

    Add background music

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if(gameObject == null) {
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+}
