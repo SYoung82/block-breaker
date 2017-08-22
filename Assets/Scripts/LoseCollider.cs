b38f9f143ddfdc7672f883e997884d805fc3cc15
Scott Young <young.scottw@gmail.com>
2017-08-22 11:55:05 -0500

    Clean up unneccessary assets, set resolution, and handle end game when trigger occurs

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LoseCollider : MonoBehaviour {
+	public LevelManager levelManager;
 	void OnTriggerEnter2D(Collider2D collider) {
 		print("Trigger");
-	}
-
-	void OnCollisionEnter2D(Collision2D collision) {
-		print("Collision");
+		levelManager.LoadLevel("Win");
 	}
 }
