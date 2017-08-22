f264b9de5e4552b82dd23e1fd592d78f582118dd
Scott Young <young.scottw@gmail.com>
2017-08-22 10:46:05 -0500

    Make MusicPlayer static to avoid restarting each scene

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour {
-
+	static MusicPlayer instance = null;
 	// Use this for initialization
 	void Start () {
-		if(gameObject == null) {
+		if(instance != null) {
+			Destroy(gameObject);
+			print("Duplicate music player self-destructing");
+		}
+		else {
+			instance = this;
 			GameObject.DontDestroyOnLoad(gameObject);
 		}
 	}
