c4d423d6b92e094fb2657701750dac2c6b8276a3
Scott Young <young.scottw@gmail.com>
2017-08-22 18:39:00 -0500

    Build brick sprite sheet for damaged bricks

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -7,21 +7,16 @@ public class Brick : MonoBehaviour {
 	public int maxHits;
 	private int timesHit;
 	private LevelManager levelManager;
+	
 	void Start() {
 		timesHit = 0;
 		levelManager = GameObject.FindObjectOfType<LevelManager>();
 	}
 
-	void OnCollisionEnter2D() {
+	void OnCollisionExit2D() {
 		timesHit++;
-		if(timesHit == maxHits) {
-			Destroy(this.gameObject);
+		if(timesHit >= maxHits) {
+			Destroy(gameObject);
 		}
-		SimulateWin();
-	}
-
-	// TODO Remove this method once we can actually win!
-	void SimulateWin() {
-		levelManager.LoadNextLevel();
 	}
 }
