67de866419ecc86922600d6a817abc72b4bdb261
Scott Young <young.scottw@gmail.com>
2017-08-22 13:57:45 -0500

    Add 2 and 3 hit prefab bricks, handle collision in Brick.cs

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+	public int maxHits;
+	private int timesHit;
+
+	void Start() {
+		timesHit = 0;
+	}
+
+	void OnCollisionEnter2D() {
+		timesHit++;
+	}
+}
