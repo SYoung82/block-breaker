5e10dcebe49f3a06ec4bc0e4fe1c37125b1e1af0
Scott Young <young.scottw@gmail.com>
2017-08-22 12:59:54 -0500

    Set up paddle movement using mouse position

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mousePosInBlocks = Mathf.Clamp((Input.mousePosition.x / Screen.width) * 16 - 0.5f, 0f, 15f);
+		Vector3 paddlePos = new Vector3(mousePosInBlocks, this.transform.position.y, this.transform.position.z);
+		this.transform.position = paddlePos;
+	}
+}
