a6758325fd59fc447832092e593af6a35461d6cc
Scott Young <young.scottw@gmail.com>
2017-08-24 16:46:47 -0500

    Add auto paddle movement for testing purposes

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,9 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Paddle : MonoBehaviour {
-	void Update () {
+
+	public bool autoPlay = false;
+	private Ball ball;
+
+	void Start() {
+		ball = GameObject.FindObjectOfType<Ball>();
+	}
+
+	void Update() {
+		if(!autoPlay) {
+			MoveWithMouse();
+		}
+		else {
+			AutoPlay();
+		}
+	}
+
+	void MoveWithMouse() {
 		float mousePosInBlocks = Mathf.Clamp((Input.mousePosition.x / Screen.width) * 16 - 0.5f, 0f, 15f);
 		Vector3 paddlePos = new Vector3(mousePosInBlocks, this.transform.position.y, this.transform.position.z);
+		this.transform.position = paddlePos;
+	}
+
+	void AutoPlay() {
+		Vector3 paddlePos = new Vector3(0f, this.transform.position.y, 0f);
+		Vector3 ballPos = ball.transform.position;
+		paddlePos.x = Mathf.Clamp(ballPos.x, 0f, 15f);
 		this.transform.position = paddlePos;
 	}
 }
